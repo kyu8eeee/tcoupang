43eebf0a2048eade9af29f0f12472dbe895d356d
kyu8eeee <130030554+kyu8eeee@users.noreply.github.com>
2023-04-09 00:03:42 +0900

    Add files via upload

---
+++ 0408.js
@@ -0,0 +1,29 @@
+const popupClosed = true;
+const popup = document.querySelector(".my-popup-new");
+const link = popup.querySelector(".popup-link");
+
+setTimeout(function () {
+  if (popupClosed) {
+    popup.style.display = "block";
+  }
+}, 3000);
+
+link.addEventListener("click", function () {
+  popupClosed = false;
+  setTimeout(function () {
+    if (!window.closed) {
+      popup.style.display = "none";
+    }
+  }, 3000);
+});
+
+window.addEventListener("popstate", function (event) {
+  if (
+    event.state &&
+    event.state.isLinkPage &&
+    event.state.timeSpent >= 3000
+  ) {
+    popupClosed = false;
+    popup.style.display = "none";
+  }
+});
